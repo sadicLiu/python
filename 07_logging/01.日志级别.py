1aa82951f742cbf8dfdbfc035fb5b34120b2c625
sadicLiu <190978752@qq.com>
2020-02-10 19:59:27 +0800

    01 done

---
+++ 07_logging/01.日志级别.py
@@ -0,0 +1,13 @@
+import logging
+
+# 日志级别: debug,info,warning,error,critical
+
+logging.basicConfig(level=logging.WARNING, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger("logger")
+
+logger.info("Start print log")
+logger.debug("Do something")
+logger.warning("Something maybe fail.")
+logger.error("Error occured")
+logger.critical("Program collapse")
+logger.info("Finish")
